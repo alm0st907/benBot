7550222d667c82986788cf54388343155c48124b
dak0ta907 <garrett.rudisill@gmail.com>
2018-01-03 20:44:33 -0800

    printing a subreddit shortlink

--- prawTest.py
+++ prawTest.py
@@ -2,13 +2,23 @@ import praw
 
 bot = praw.Reddit(user_agent='ben_bot',client_id='GJQk28rd1Bp-DA',client_secret='rjofYTFzwpodjztC0PJU0-TRipI',username='',password='')
 
-subreddit = bot.subreddit('space')
-comments = subreddit.stream.comments()
+#code returns hot 100 posts()
+subreddit = bot.subreddit('dankmemes')
 
-for comment in comments:
-    text = comment.body # Fetch body
-    author = comment.author # Fetch author
-    if 'mars' in text.lower():
-        # Generate a message
-       print("test message")
-       break
+#this is a list of submissions
+submissions = subreddit.hot()
+
+#iterates to the next submission; submission is a given data type?
+#starts with the first post,loop ieratres to the 3rd post
+i=0
+for i in range(0,3):
+    submission=submissions.next()
+
+#print the submission title/link
+print(submission.title)
+print(submission.shortlink)
+
+# for submission in submissions:
+#     print(submission.title)
+#     print(submission.url)
+#     break
