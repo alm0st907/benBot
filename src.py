b0450e63ff89340ce22f1efbed2c3288542ee311
dak0ta907 <garrett.rudisill@gmail.com>
2018-01-04 12:40:10 -0800

    scan the follower list code, ability to dynamically choose a user name

--- src.py
+++ src.py
@@ -1,10 +1,31 @@
 import tweepy
 import reddit #my reddit praw code file
 
+#need to figure out this code as compared to the get_api code
+consumer_key = 'RzjIudiw6pddHytAJDDRDPxlz'
+consumer_secret = '3HZNFEvPX1aYgoXYq80GvXTLiAmk3bpiyxKgoIR5IVlAWAJDhA'
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, retry_count=3, retry_delay=60)
+
+
+
+
+
 def get_api(cfg):
   auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
   auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
   return tweepy.API(auth)
+
+#this is code ripped from a stackex post, no idea if it works yet
+def get_followers(user_id):
+    users = []
+    page_count = 0
+    for user in tweepy.Cursor(api.followers, id=user_id, count=200).pages():
+        page_count += 1
+        print 'Getting page {} for followers'.format(page_count)
+        users.extend(user)
+    return users
+
 
 def main():
   # Fill in the values noted in previous step here
@@ -22,7 +43,26 @@ def main():
   title=reddit.get_title()
   
 
-  api = get_api(cfg)
+  bot = get_api(cfg)
+
+  #WIP code
+  myUser=bot.me()
+  #user = myUser.id
+  user=myUser.id
+  
+  #just prints user id
+  #print(user)
+
+  follower_list=[]
+  #gets a list of users
+  follower_list=get_followers(user)
+
+  #pop off a user object 
+  temp=follower_list.pop()
+
+  #access the screen name of a user object
+  #print(temp.screen_name)
+  
 
  #MEME FLING CHOICE SELECTOR
   handle="blank_string"
@@ -37,18 +77,22 @@ def main():
       handle = "@IanStacks543"
       status=True
     else:
-      handle="@alm0st907"
-      status=True
+      if handle == temp.screen_name: #checking if the name matches the temp
+        status=True
       pass
  #MEME FLING CHOICE SELECTOR
 
   #Formats the tweet with the pulled title, pulled link, identifies the bot, then throws the @
-  tweet = title + '\n' + link + "\nBy Memebot 9000 (wip)\n"+handle
-  print(tweet)
+  tweet = title + '\n' + link + "\nBy Memebot 9000 (wip)\n"+'@'+handle
+
+  #just a debug line of code, see what the tweet is
+  #print(tweet)
   
   #DROP A TRAIN ON EM EDGAR
-  status = api.update_status(status=tweet) 
+  status = bot.update_status(status=tweet) 
   # Yes, tweet is called 'status' rather confusing
 
 if __name__ == "__main__":
   main()
+
+
