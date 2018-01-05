8af7edd74066c1edc91b3c99ea177c2f12d09a09
dak0ta907 <garrett.rudisill@gmail.com>
2018-01-04 17:46:55 -0800

    added a template config.ini as a template, updated the configtest code

--- configTest.py
+++ configTest.py
@@ -3,4 +3,10 @@ import ConfigParser #import library
 parse = ConfigParser.ConfigParser()
 parse.read('config.ini')
 
-print parse.get('info','test') #make a section in brackets
+#make a section in brackets
+#print parse.get('info','test') 
+
+print parse.get('info','consumer_key')
+print parse.get('info','consumer_secret')
+print parse.get('info','access_token')
+print parse.get('info','access_token_secret')
